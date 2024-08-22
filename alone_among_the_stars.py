85a38c484a894fc25d1c786ba9bdc2d741a96529
Philippe Lemaire <philippe.lemaire@protonmail.com>
2024-08-22 20:54:29 +0200

    some bold in prompts descriptions

--- alone_among_the_stars.py
+++ alone_among_the_stars.py
@@ -61,15 +61,15 @@ prompts_ranks = {
 }
 
 prompts_suits = {
-    "Diamonds": """Living beings: People like or unlike you, fish,
+    "Diamonds": """**Living beings:** People like or unlike you, fish,
 dinosaurs, wolves, birds, giant insects, etc.""",
-    "Clubs": """Plants and other immobile forms of life: Towering
+    "Clubs": """**Plants and other immobile forms of life:** Towering
 trees, carnivorous pitchers, giant ferns, glowing weeds,
 floating flowers, oozing mushrooms, etc.""",
-    "Hearts": """Ruins: Mysterious obelisks, vine-covered temples,
+    "Hearts": """**Ruins:** Mysterious obelisks, vine-covered temples,
 abandoned dwellings for people bigger than you, a wrecked
 spaceship, etc.""",
-    "Spades": """Natural phenomenon: Huge crystal formations,
+    "Spades": """**Natural phenomenon:** Huge crystal formations,
 mirages, vividly colored lightning, strange clouds, rocks
 eroded in strange shapes, veins of precious metals, etc.""",
 }
