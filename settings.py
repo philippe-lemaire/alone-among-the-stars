ead140eba7da4da4603d728c03109347fb8295d3
Philippe Lemaire <philippe.lemaire@protonmail.com>
2024-08-26 19:04:20 +0200

    cleaner export path and variables

--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ import os
 
 home_dir = os.path.expanduser("~")
 text_files_dir = "alone_among_the_stars"
+export_path = os.path.join(home_dir, text_files_dir)
+# create export folder
+os.makedirs(export_path, exist_ok=True)
+
 now = datetime.now()
-filename = os.path.join(home_dir, text_files_dir, f"{now.date()}_{now.time()}.md")
+filename = os.path.join(export_path, f"{now.date()}_{now.time()}.md")
 editor = "vim"
