0da9558be2957370cd1765cf442a44a045d16e6b
Philippe Lemaire <phil@plemaire.net>
2025-05-19 17:45:20 +0200

    changed output folder and ignored it

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 import os
 
-home_dir = os.path.expanduser("~")
-text_files_dir = "alone_among_the_stars"
-export_path = os.path.join(home_dir, text_files_dir)
+base_dir = os.getcwd()
+text_files_dir = "output"
+export_path = os.path.join(base_dir, text_files_dir)
 # create export folder
 os.makedirs(export_path, exist_ok=True)
 
